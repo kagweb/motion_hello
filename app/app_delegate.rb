9774d139377cda256ceae8fee52ac0255bf0df10
Ichiro Kimura <kag.web@gmail.com>
2014-04-10 19:48:32 +0900

    webview化

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,7 +1,7 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.rootViewController = HelloViewController.new
+    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(MainViewController.new)
     @window.makeKeyAndVisible
     true
   end
